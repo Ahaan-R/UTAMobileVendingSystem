408bcb2a0f6f49fcb1a1edf4550a86861740bbb7
Nischith <nischithp@gmail.com>
2020-05-02 15:09:45 -0500

    Added my files to Manifest and added UserId to viewOrders()

--- app/src/main/java/com/example/utamobilevendingsystem/HomeScreens/OperatorHomeScreen.java
+++ app/src/main/java/com/example/utamobilevendingsystem/HomeScreens/OperatorHomeScreen.java
@@ -225,26 +225,7 @@ public class OperatorHomeScreen extends RegistrationHelper {
                 viewLocationList();
                 return true;
             case R.id.menu_view_orders:
-                role= role+"OrderDetails";
-                if (role == "user"){
-                    try {
-                        Class<?> cls = Class.forName("com.example.utamobilevendingsystem.users."+role);
-                        Intent homeIntent = new Intent(this, cls);
-                        startActivity(homeIntent);
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-                else{
-                    try {
-                        Class<?> cls = Class.forName("com.example.utamobilevendingsystem."+role);
-                        Intent homeIntent = new Intent(this, cls);
-                        startActivity(homeIntent);
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-
+                viewOrders();
                 return true;
             case R.id.Optr_vehicledetails:
                 vehicleSearch_optr();
